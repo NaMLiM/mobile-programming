a1d1a2e0810e3cfd96549b3ace8dc20ba7c1298d
NaMLiM <anamnafiul99@gmail.com>
2021-12-02 19:34:21 +0700

    update 1

--- admin/list_laundry.php
+++ admin/list_laundry.php
@@ -2,8 +2,8 @@
 include "../koneksi.php";
 $pelanggan = $_POST['pelanggan'];
 
-$get_laundry_data = mysqli_query($koneksi, "SELECT id,jumlah_pakaian,alamat,status_pengerjaan FROM pelanggan_laundry WHERE pelanggan = '$pelanggan'");
+$get_laundry_data = mysqli_query($koneksi, "SELECT * from pelanggan_laundry INNER JOIN user on pelanggan_laundry.pelanggan = user.id");
 
 while ($data = mysqli_fetch_array($get_laundry_data)) {
-    echo $data['id'] . "|" . $data['jumlah_pakaian'] . "|" . $data['alamat'] . "|" . $data['status_pengerjaan'] . ",";
+    echo $data['id'] . "|" . $data['nama'] . "|" . $data['jumlah_pakaian'] . "|" . $data['alamat'] . "|" . $data['status_pengerjaan'] . ",";
 }
